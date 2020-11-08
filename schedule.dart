240ec9b51d2b273f7686cd5eacaa891f540a07d3
Navya Garg <34911308+navgarg@users.noreply.github.com>
2020-11-08 18:09:00 +0530

    Create schedule.dart

---
+++ schedule.dart
@@ -0,0 +1,185 @@
+var listOfEvents = [
+  Event(
+      name: 'Hardware',
+      timing: '0000HRS',
+      date: DateTime(2020, 11, 15),
+      information:
+          'Power up your cores and overclock that grey matter, because you’re in for some serious hands-on competition. Identify components belonging to a plethora of devices and show off your know-how about hardware.'),
+  Event(
+      name: 'ExML',
+      timing: '1800HRS',
+      date: DateTime(2020, 11, 15),
+      information:
+          'Tired of the usual plug-and-chug in contests? Look no further, we bring you a cross-disciplinary Machine Learning event where there will be a rich variety of unique yet equally mesmerising machine learning paradigms on display and in use.'),
+  Event(
+      name: 'Opening',
+      timing: '1700HRS',
+      date: DateTime(2020, 11, 15),
+      information:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu facilisi condimentum non aliquam commodo vitae, nec nam ac. Lobortis sit id lectus ac blandit. Lorem duis eget faucibus consequat fermentum eu. Porta viverra condimentum feugiat ipsum euismod.'),
+  Event(
+      name: 'Quiz Prelims',
+      timing: '1500HRS',
+      date: DateTime(2020, 11, 16),
+      information:
+          'An enigmatic quiz based on the field of computers, technology, and recent events in the world of IT — a battleground for quizzers to test their wits under extreme pressure.'),
+  Event(
+      name: 'Girls in Tech Tutorial',
+      timing: '1800HRS',
+      date: DateTime(2020, 11, 16),
+      information:
+          'At Exun Clan, we feel that a diverse set of participants is important for the success of our event. With that in mind, Exun 2020 will feature an exciting surprise event for girls in collaboration with Women Who Code Delhi.'),
+  Event(
+      name: 'Crossword Prelims',
+      timing: '1800HRS',
+      date: DateTime(2020, 11, 16),
+      information:
+          'A cryptic crossword based on wit, presence of mind, and an excellent sense of humour. All answers will be related to the fields of computers, technology, and recent events in the field of IT.'),
+  Event(
+      name: '[RoboKnights] BISCUITS',
+      timing: '1800HRS',
+      date: DateTime(2020, 11, 16),
+      information:
+          'Race your problem solving skills in circuits and electronics in a new cryptic hunt format.'),
+   Event(
+      name: 'Designathon',
+      timing: '0000HRS',
+      date: DateTime(2020, 11, 17),
+      information:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu facilisi condimentum non aliquam commodo vitae, nec nam ac. Lobortis sit id lectus ac blandit. Lorem duis eget faucibus consequat fermentum eu. Porta viverra condimentum feugiat ipsum euismod.'),
+     Event(
+      name: 'Hackathon',
+      timing: '0000HRS',
+      date: DateTime(2020, 11, 17),
+      information:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu facilisi condimentum non aliquam commodo vitae, nec nam ac. Lobortis sit id lectus ac blandit. Lorem duis eget faucibus consequat fermentum eu. Porta viverra condimentum feugiat ipsum euismod.'),
+     Event(
+      name: 'Unreality',
+      timing: '0000HRS',
+      date: DateTime(2020, 11, 17),
+      information:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu facilisi condimentum non aliquam commodo vitae, nec nam ac. Lobortis sit id lectus ac blandit. Lorem duis eget faucibus consequat fermentum eu. Porta viverra condimentum feugiat ipsum euismod.'),
+ Event(
+      name: 'Lockout',
+      timing: '1600HRS',
+      date: DateTime(2020, 11, 17),
+      information:
+          'Another new addition to Exun 2020, Lockout is a competitive programming event, where participants go head to head in a knockout style lockout tournament featuring handpicked problems.'),
+ Event(
+      name: 'Surprise Prelims',
+      timing: '1600HRS',
+      date: DateTime(2020, 11, 17),
+      information:
+          'Participants will face unconventional challenges linked to the world of technology. Nothing is guaranteed.'),
+Event(
+      name: 'Gaming Semis',
+      timing: '1900HRS',
+      date: DateTime(2020, 11, 17),
+      information:
+          'The prelims will be held online in multiple batches, on 10th, 11th and 12th November. Games for the prelims will be revealed a week before on the Discord server. All further details will be given on the official Domain2+ Discord server.'),
+Event(
+      name: 'Gaming Semis',
+      timing: '1900HRS',
+      date: DateTime(2020, 11, 17),
+      information:
+          'The prelims will be held online in multiple batches, on 10th, 11th and 12th November. Games for the prelims will be revealed a week before on the Discord server. All further details will be given on the official Domain2+ Discord server.'),
+Event(
+      name: 'Turing',
+      timing: '1500HRS',
+      date: DateTime(2020, 11, 18),
+      information:
+          'Computational linguists are interested in providing computational models of various kinds of linguistic phenomena, and it is this field that we invite you to explore in Turing Test.'),
+Event(
+      name: 'Surprise Finals',
+      timing: '1600HRS',
+      date: DateTime(2020, 11, 18),
+      information:
+          'Participants will face unconventional challenges linked to the world of technology. Nothing is guaranteed.'),
+  Event(
+      name: 'LessWrong',
+      timing: '0000HRS',
+      date: DateTime(2020, 11, 19),
+      information:
+          'LessWrong is a first of a kind competitive programming event combining the excitement of short contests with the forethought necessary for preparing problems.'),
+Event(
+      name: 'GD Semis',
+      timing: '1600HRS',
+      date: DateTime(2020, 11, 19),
+      information:
+          'A test of creativity, logical skills and wit.'),
+Event(
+      name: 'Surprise Finals',
+      timing: '1600HRS',
+      date: DateTime(2020, 11, 19),
+      information:
+          'Participants will face unconventional challenges linked to the world of technology. Nothing is guaranteed.'),
+Event(
+      name: 'Gaming Finals',
+      timing: '1900HRS',
+      date: DateTime(2020, 11, 19),
+      information:
+          'The prelims will be held online in multiple batches, on 10th, 11th and 12th November. Games for the prelims will be revealed a week before on the Discord server. All further details will be given on the official Domain2+ Discord server.'),
+Event(
+      name: 'GD Finals',
+      timing: '1900HRS',
+      date: DateTime(2020, 11, 20),
+      information:
+          'A test of creativity, logical skills and wit.'),
+Event(
+      name: 'Make-A-Thon Finals',
+      timing: '1900HRS',
+      date: DateTime(2020, 11, 20),
+      information:
+          'Build an innovative solution to solve real-life problems using Robotics and Engineering.'),
+Event(
+      name: 'Build Demos',
+      timing: '2000HRS',
+      date: DateTime(2020, 11, 20),
+      information:
+          'Build is an event that requires creative participants from a team to come together and build solutions to pervasive problems. Build at Exun 2020 will feature three separate sub-events, the Designathon, Hackathon, and Unreality (GameDev).'),
+Event(
+      name: 'SpaceTech',
+      timing: '1200HRS',
+      date: DateTime(2020, 11, 21),
+      information:
+          'Exun Clan is collaborating with the Delhi Chapter of the prestigious National Space Society (USA) to give students the opportunity to explore the realms of space and its concurrence with technology.'),
+Event(
+      name: 'LessWrong',
+      timing: '1200HRS',
+      date: DateTime(2020, 11, 21),
+      information:
+          'LessWrong is a first of a kind competitive programming event combining the excitement of short contests with the forethought necessary for preparing problems.'),
+Event(
+      name: 'Cubing Finals',
+      timing: '1200HRS',
+      date: DateTime(2020, 11, 21),
+      information:
+          'It’s always the small pieces that make the big picture. Participants can individually compete in 3 events: Pyraminx, 2x2 Cubing and 3X3 Cubing.'),
+Event(
+      name: 'Crossword Finals',
+      timing: '1200HRS',
+      date: DateTime(2020, 11, 22),
+      information:
+          'A cryptic crossword based on wit, presence of mind, and an excellent sense of humour. All answers will be related to the fields of computers, technology, and recent events in the field of IT.'),
+Event(
+      name: 'Cubing Finals',
+      timing: '1200HRS',
+      date: DateTime(2020, 11, 22),
+      information:
+          'It’s always the small pieces that make the big picture. Participants can individually compete in 3 events: Pyraminx, 2x2 Cubing and 3X3 Cubing.'),
+Event(
+      name: 'Quiz Finals',
+      timing: '1200HRS',
+      date: DateTime(2020, 11, 22),
+      information:
+          'An enigmatic quiz based on the field of computers, technology, and recent events in the world of IT — a battleground for quizzers to test their wits under extreme pressure.'),
+  Event(
+      name: 'Closing',
+      timing: '1700HRS',
+      date: DateTime(2020, 11, 22),
+      information:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu facilisi condimentum non aliquam commodo vitae, nec nam ac. Lobortis sit id lectus ac blandit. Lorem duis eget faucibus consequat fermentum eu. Porta viverra condimentum feugiat ipsum euismod.')]
+  
+  
+  
+  
